26f8d5add0725a8f6728a7f7d077d32dc3b80d03
Firestorm Developer <FirestormDevelopers@gmail.com>
2014-07-30 03:08:00 -0700

    Inital Commit

---
+++ Source/Firestorm/Public/Firestorm.h
@@ -0,0 +1,11 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FIRESTORM_H__
+#define __FIRESTORM_H__
+
+#include "Engine.h"
+
+#define COLLISION_PROJECTILE	ECC_GameTraceChannel1
+
+
+#endif
